8ca784844fc4239e5e5f95aa51b54098feed17ba
Aldrin <aldrinbrunodjourobi@gmail.com>
2025-07-29 17:12:26 +0100

    premier deploiement sur railway

---
+++ cfehome/cfehome/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def hello_world(request):
+    return render(request,"hello_world.html",{})
+
+def  health_view(request):
+    return HttpResponse("ok")
