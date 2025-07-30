fbc83edb37ed596d5d33e6a11a74a5456334f456
Aldrin <aldrinbrunodjourobi@gmail.com>
2025-07-30 09:44:21 +0100

    add database

--- cfehome/cfehome/settings.py
+++ cfehome/cfehome/settings.py
@@ -103,6 +103,14 @@ DATABASES = {
     }
 }
 
+DATABASE_URL =config("DATABASE_URL",default="",cast=str)
+if DATABASE_URL :
+    import dj_database_url 
+    if DATABASE_URL.startswith('postgres//') or DATABASE_URL.startswith('posgresql://'):
+        DATABASES={
+        "default":dj_database_url.config(
+            default=DATABASE_URL,) }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
